8dce9d45b771916c7b48914e70be3f02b72e2e6e
Santiago Aragon <santiagoas77@gmail.com>
2020-03-07 13:33:36 -0600

    Conexion con Facebook para login nativo terminado y funcionando. Creacion de la UI de la vista principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private fb: Facebook) {
+
+  }
+
+  loginWithFB(){
+    this.fb.login(['email', 'public_profile', 'user_friends'])
+    .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+    .catch(e => console.log('Error logging into Facebook', e));
+  }
 
 }
