61b836d0c11a5d6e463ac136f5e92d74bbe78a80
Santiago Aragon <santiagoas77@gmail.com>
2020-03-09 23:26:14 -0600

    Cambios minimos a la UI de home y animaciones de transicion para tabs

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
 })
-export class TabsPage implements OnInit {
+export class TabsPage {
 
-  constructor() { }
+  loaded:   boolean = false;
+  tabIndex: number  = 0;
 
-  ngOnInit() {
+  constructor(private nativePageTransitions: NativePageTransitions,
+              public navCtrl: NavController) { }
+
+
+  private getAnimationDirection(index:number):string {
+    var currentIndex = this.tabIndex;
+
+    this.tabIndex = index;
+
+    switch (true){
+      case (currentIndex < index):
+        return('left');
+      case (currentIndex > index):
+        return('right');
+    }
+  }
+
+  public transition(e:any):void {    
+    let options: NativeTransitionOptions = {
+     direction:this.getAnimationDirection(e.index),
+     duration: 200,
+     slowdownfactor: -1,
+     slidePixels: 0,
+     iosdelay: 20,
+     androiddelay: 0,
+     fixedPixelsTop: 0,
+     fixedPixelsBottom: 48
+    };
+
+    if (!this.loaded) {
+      this.loaded = true;
+      return;
+    }
+
+    this.nativePageTransitions.slide(options);
+
   }
 
 }
